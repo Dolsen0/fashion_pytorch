b3059429116c38a4182e64f76effb0c403fa6747
dolsen0 <derek.olsen@gmail.com>
2023-02-23 18:14:45 -0500

    defined nn model

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+
 training_data = datasets.FashionMNIST(
     root="data",
     train=True,
@@ -28,3 +29,26 @@ for X, y in test_dataloader:
     print(f"Shape of y:  {y.shape} {y.dtype}")
     break
 
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using {device} device")
+
+#define the model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
